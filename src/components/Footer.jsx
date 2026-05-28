0e173b766cb17c1dd91866fcfc33f061a5771b6d
Joaco <joacoabecian@gmail.com>
2026-05-28 12:01:08 -0300

    mejorar diseno x2

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,8 @@
 export default function Footer() {
   return (
     <footer className="bg-gray-900 text-gray-400 text-center text-sm py-6 mt-12">
-      <p>Datos provistos por <span className="text-white font-semibold">TMDB</span></p>
-      <p className="mt-1">TP3 — Programación 3</p>
+      <p>TP-Movies <span className="text-white font-semibold">terminar footer</span></p>
+     
     </footer>
   );
 }
